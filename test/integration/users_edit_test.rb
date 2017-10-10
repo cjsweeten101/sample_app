68371d4aa4f7fe861ce1a9b40cb563d8d2f17897
Clayton <cjsweeten101@gmail.com>
2017-10-10 15:31:31 -0600

    test for forwarding url

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -20,6 +20,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
 	test "successful edit with friendly forwarding" do
 		get edit_user_path(@user)
+		assert_equal edit_user_url(@user), session[:forwarding_url] 
 		log_in_as(@user) 
 		assert_redirected_to edit_user_url(@user)
 		name = "Foo Bar"
@@ -32,6 +33,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_redirected_to @user
     @user.reload
     assert_equal name, @user.name
-    assert_equal email, @user.email                                         
+    assert_equal email, @user.email                                       
 	end
 end
